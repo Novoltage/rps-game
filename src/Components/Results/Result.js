cdccfa583aa160814314ec9339a06183d3ce13a9
Kevin Bonifacio <kevin>
2023-11-04 18:38:50 -0400

    result component basics

--- src/Components/Results/Result.js
+++ src/Components/Results/Result.js
@@ -1,9 +1,24 @@
+import '..result/result.scss';
+import ScoreCard from '../ScoreCard/ScoreCard';
+
 
 function result() {
 
   return (
-    <section className='Result'>
-      
+    <section className='result'>
+      <ScoreCard score={score}/>
+      <div className='result__moves'>
+        
+      </div>
+      <div className='result__result'>
+
+      </div>
+      <div className='result__play'>
+
+      </div>
+      <div className='result__rules'>
+                <button className='game__rules-button'>RULES</button>
+            </div>
     </section>
   );
 }
